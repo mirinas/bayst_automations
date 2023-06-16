533d885cb959f4116df3002f976ca24d40372a02
mirinas <augustas.mirinas@gmail.com>
2023-06-17 02:14:41 +0300

    Favicon fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ export default function About() {
                 <title>About</title>
                 <meta name="description" content="Automation Developer for ecommerce" />
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico?" />
             </Head>
             <main className={styles.main}>
 
--- src/pages/services.js
+++ src/pages/services.js
@@ -45,7 +45,7 @@ export default function Services() {
                 <title>Services</title>
                 <meta name="description" content="Automation Developer for ecommerce" />
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico?" />
             </Head>
 
             <main className={styles.main}>
